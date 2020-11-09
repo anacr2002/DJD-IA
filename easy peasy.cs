ed580b0c62a33ac5410377bde911b953f959005e
anacr2002 <72752737+anacr2002@users.noreply.github.com>
2020-11-09 17:43:30 +0000

    Add files via upload

---
+++ easy peasy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lives = 3;
+            Console.WriteLine("You have" + lives + "lives use them wisely");
+        }
+    }
+}
---
+++ forAzeoth.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String Name = "Tyrade Whisperwind";
+            int Level = 40;
+            String Race = "Night elf";
+            Char Gender = F;
+            String Faction = "Aliance";
+            String Class="Hunter";
+
+            Console.WriteLine ("Your Characters name is"+ Name );
+            Console.WriteLine ("She's int the level"+Level);
+            Console.WriteLine("Her race is" + Race);
+            Console.WriteLine("Gender"+Gender);
+            Console.WriteLine("Fraction" + Faction);
+            Console.WriteLine("Class" + Class);
+
+
+           
+        }
+    }
+}
