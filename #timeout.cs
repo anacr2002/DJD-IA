eca49ba3bcd756460857b1069e6aa1d3cf874719
anacr2002 <72752737+anacr2002@users.noreply.github.com>
2020-11-21 20:17:26 +0000

    Add files via upload

---
+++ #timeout.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 99; i > 0; i--)
+            {
+                Console.WriteLine(i);
+                Console.WriteLine("Time over");
+            }
+        }
+    }
+}
